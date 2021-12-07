d15ce7402c807f5737a7d3a1756f541a58faafc7
WhoRun <473189493@qq.com>
2021-12-07 13:06:14 +0800

    feat: 完善header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,23 @@
 <template>
   <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display: flex">
-    <div style="width: 200px">后台管理</div>
+    <div style="width: 200px; padding-left: 30px; font-weight: bold; color: dodgerblue">后台管理</div>
     <div style="flex: 1"></div>
-    <div style="width: 100px">下拉框</div>
+    <div style="width: 100px">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          下拉菜单
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>Action 1</el-dropdown-item>
+            <el-dropdown-item>Action 2</el-dropdown-item>
+            <el-dropdown-item >Action 3</el-dropdown-item>
+            <el-dropdown-item disabled>Action 4</el-dropdown-item>
+            <el-dropdown-item >Action 5</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
