f0276188abe83abb98f2a3dc3a1cace51db6d88b
WhoRun <473189493@qq.com>
2021-12-07 15:35:02 +0800

    feat: 主页功能完善

--- src/main.js
+++ src/main.js
@@ -7,4 +7,4 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).use(store).use(router).use(ElementPlus).use(ElementPlus,{locale: zhCn,}).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus).use(ElementPlus,{locale: zhCn, size:'small'}).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,52 @@
 <template>
-  <el-table :data="tableData" style="width: 100%" height="250">
-    <el-table-column fixed prop="date" label="Date" width="150" />
-    <el-table-column prop="name" label="Name" width="120" />
-    <el-table-column prop="state" label="State" width="120" />
-    <el-table-column prop="city" label="City" width="320" />
-    <el-table-column prop="address" label="Address" width="600" />
-    <el-table-column prop="zip" label="Zip" width="120" />
-  </el-table>
+  <div style="width: 80%">
+    <div
+        id="function"
+        style="margin: 10px 0"
+    >
+      <el-button type="primary">新增</el-button>
+      <el-button type="primary">导入</el-button>
+      <el-button type="primary">导出</el-button>
+    </div>
+
+    <div
+        id="search"
+        style="margin: 10px 0"
+    >
+      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%">新增</el-input>
+      <el-button type="primary" style="margin-left: 10px;">查询</el-button>
+
+    </div>
+    <el-table :data="tableData" style="width: 100%" height="250" stripe>
+      <el-table-column fixed prop="date" label="Date" width="150"  sortable="true"/>
+      <el-table-column prop="name" label="Name" width="120" />
+      <el-table-column prop="state" label="State" width="120" />
+      <el-table-column prop="city" label="City" width="320" />
+      <el-table-column prop="address" label="Address" width="600" />
+      <el-table-column prop="zip" label="Zip" width="120" />
+      <el-table-column fixed="right" label="Operations" width="120">
+        <template #default>
+          <el-button type="text" size="small" @click="handleClick"
+          >Detail</el-button
+          >
+          <el-button type="text" size="small">Edit</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div style="margin: 10px 0">
+      <el-pagination
+          v-model:currentPage="currentPage"
+          :page-sizes="[5, 10, 20]"
+          :page-size="10"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+      >
+      </el-pagination>
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -72,6 +112,19 @@ export default {
           zip: 'CA 90036',
         },
       ],
+      currentPage: 1,
+      total: 10,
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log('click')
+    },
+    handleSizeChange() {
+
+    },
+    handleCurrentChange() {
+
     }
   },
 }
