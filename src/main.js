0e8c2a353aa28bce882fd7d0b259428c2fd05bff
WhoRun <473189493@qq.com>
2021-12-07 14:47:43 +0800

    feat: el组件配置中文

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import store from './store'
 import "@/assets/css/global.css"
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus).use(ElementPlus,{locale: zhCn,}).mount('#app')
